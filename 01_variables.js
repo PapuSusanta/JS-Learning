64a5ae4670692b5776d50904f5c35b2b3fe774dd
PapuSusanta <susantamaji711@gmail.com>
2024-05-01 23:57:54 +0530

    addJsFiles

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234567890
+let accountEmail = "test@email.com"
+var accountPassword = "123"
+accountCity = "Kolkata"
+let accountState
+
+// accountId = 9876543210 //not allowed
+
+accountEmail = "<EMAIL>"
+accountPassword = "<PASSWORD>"
+accountCity = "Mumbai"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
